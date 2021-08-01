76231acaaa3fa06685a62b243f9bd8ac82458eb6
yujin-dev <qlcnal0211@gmail.com>
2021-08-01 23:55:11 +0900

    pipeline 정리

--- model/__init__.py
+++
@@ -1,7 +0,0 @@
-import gensim
-from model.performance import *
-
-def LDAmodel(id2word, corpus, num_topics=20, passes=10, per_word_topics=True):
-    LDAmodel= gensim.models.ldamodel.LdaModel(corpus, num_topics=num_topics, id2word=id2word, passes=passes,
-                                              per_word_topics=per_word_topics)
-    return LDAmodel
--- scripts/utils.py
+++
@@ -1,5 +0,0 @@
-def header(txt, md=True):
-    if md:
-        print(f"\n### {txt}  ")
-    else:
-        print(f"\n◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆ {txt} ◆◆◆◆◆◆◆◆◆◆◆◆◆◆◆")
