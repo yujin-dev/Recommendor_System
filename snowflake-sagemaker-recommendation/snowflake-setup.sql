f6f7daf7403fd4fd959d47fcffd0725e089182f5
yujin-dev <qlcnal0211@gmail.com>
2022-09-11 00:22:10 +0900

    sagemaker usecase

--- snowflake-sagemaker-recommendation/snowflake-setup.sql
+++ snowflake-sagemaker-recommendation/snowflake-setup.sql
@@ -35,3 +35,28 @@ create table ratings_train_data_local_test as
 -- OUTPUT TABLE: top 10 recommendations for local_testing
 create or replace table user_movie_recommendations_local_test
 (USERID int, TOP_10_RECOMMENDATIONS variant);
+-- connect API Gateway with Snowflake
+use role accountadmin;
+use schema TEST.PUBLIC;
+create or replace api integration snf_recommender_api_integration
+api_provider = aws_api_gateway
+api_aws_role_arn = 'arn:aws:iam::717473574740:role/SnowflakeAPIGatewayTest'
+enabled = true
+api_allowed_prefixes = ('https://pgvn9t3vhc.execute-api.us-east-1.amazonaws.com/dev/');
+-- create external functions
+create or replace external function train_and_get_recommendations(input_table_name varchar, output_table_name varchar)
+    returns variant
+    api_integration = snf_recommender_api_integration
+    as 'https://pgvn9t3vhc.execute-api.us-east-1.amazonaws.com/dev/train';
+create or replace external function deploy_model(model_name varchar, model_url varchar)
+returns variant
+api_integration = snf_recommender_api_integration
+as 'https://pgvn9t3vhc.execute-api.us-east-1.amazonaws.com/dev/deploy';
+create or replace external function invoke_model(model_name varchar, user_id varchar, item_id varchar)
+returns variant
+api_integration = snf_recommender_api_integration
+as 'https://pgvn9t3vhc.execute-api.us-east-1.amazonaws.com/dev/invoke';
+-- grant permissions to sysadmin
+grant usage on function train_and_get_recommendations(varchar, varchar) to role sysadmin;
+grant usage on function deploy_model(varchar, varchar) to role sysadmin;
+grant usage on function invoke_model(varchar, varchar, varchar) to role sysadmin;
